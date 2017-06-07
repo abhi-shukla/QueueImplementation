8bf7f508cd2f5db9277d4a086c0328cb663c8fd8
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-07 00:31:36 -0700

    Improve Queue implementation using array

--- QueueImplementation/MyArrayQueue.cs
+++ QueueImplementation/MyArrayQueue.cs
@@ -18,33 +18,68 @@ namespace QueueImplementation
 
         public void Enqueue(T item)
         {
-            if(_size == 0)
+            if (_size == _list.Length)
             {
-                _list = new T[4];
-            }
-            else if(_size == _list.Length)
-            {                
-                var newArray = new T[_size*2];
-                _list.CopyTo(newArray, 0);
+                var newArrayLength = _size == 0 ? 4 : _size * 2;
+                T[] newArray = new T[_size * 2];
+
+                if (_size > 0)
+                {
+                    var newArrayIndex = 0;
+
+                    if (_tail < _head)
+                    {
+                        // Copy elements form head to end
+                        for (int i = _head; i < _list.Length; i++)
+                        {
+                            newArray[newArrayIndex] = _list[i];
+                            newArrayIndex++;
+                        }
+
+                        // Copy elements from start until tail
+                        for (int i = 0; i <= _tail; i++)
+                        {
+                            newArray[newArrayIndex] = _list[i];
+                            newArrayIndex++;
+                        }
+                    }
+                    else
+                    {
+                        for(int i = _head; i <= _tail; i++)
+                        {
+                            newArray[newArrayIndex] = _list[i];
+                            newArrayIndex++;
+                        }
+                    }
+
+                    _head = 0;
+                    _tail = newArrayIndex - 1;
+                }
+                else
+                {
+                    _head = 0;
+                    _tail = -1;
+                }
+
                 _list = newArray;
             }
 
-            if(_head == -1 && _tail == -1)
+            if(_tail == _list.Length -1)
             {
-                _list[++_head] = item;
-                _tail++;
-                _size++;
+                _tail = 0;
             }
             else
             {
-                _list[++_tail % _list.Length] = item;
-                _size++;
+                _tail++;
             }
+
+            _list[_tail] = item;
+            _size++;
         }
 
         public T Dequeue()
         {
-            if(_head == -1)
+            if(_size == 0)
             {
                 throw new IndexOutOfRangeException("Queue is empty");
             }
@@ -53,14 +88,44 @@ namespace QueueImplementation
             return _list[_head++ % _list.Length];
         }
 
+        public T Peek()
+        {
+            if(_size == 0)
+            {
+                throw new IndexOutOfRangeException("Queue is empty");
+            }
+
+            return _list[_head % _list.Length];
+        }
+
         public IEnumerator<T> GetEnumerator()
         {
-            return (IEnumerator<T>) _list.GetEnumerator();
+            if (_size > 0)
+            {
+                if(_tail < _head)
+                {
+                    for(int i = _head; i < _list.Length; i++)
+                    {
+                        yield return _list[i];
+                    }
+                    for(int i = 0; i<_tail; i++)
+                    {
+                        yield return _list[i];
+                    }
+                }
+                else
+                {
+                    for(int i = _head; i <= _tail; i++)
+                    {
+                        yield return _list[i];
+                    }
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return _list.GetEnumerator();
+            return GetEnumerator();
         }
     }
 }
