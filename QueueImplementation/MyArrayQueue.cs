23eee2a9af26f2836cd46018b0d1d687239a29dd
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-06 19:47:18 -0700

    Add Queue Implementation using Linked List

---
+++ QueueImplementation/MyArrayQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementation
+{
+    public class MyArrayQueue<T> : IEnumerable<T>
+    {
+        T[] _list = new T[0];
+        int _size = 0;
+
+        public int Count => _size;
+
+        public void Enqueue(T item)
+        {
+            
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>) _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
---
+++ QueueImplementation/MyLLQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementation
+{
+    public class MyLLQueue<T> : IEnumerable<T>
+    {
+        private readonly LinkedList<T> _list = new LinkedList<T>();
+
+        public int Count => _list.Count;
+
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            var temp = _list.First.Value;
+            _list.RemoveFirst();
+
+            return temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
